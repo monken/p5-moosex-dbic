8cb0994db3e4496ad3169453bfe8e79b3074baf6
Moritz Onken <onken@netcubed.de>
2010-03-13 18:30:06 +0800

    using dbic's slots was a bad idea

--- t/lib/MyApp/User.pm
+++ t/lib/MyApp/User.pm
@@ -3,6 +3,6 @@ use Moose;
 
 has [qw(last email)] => ( is => 'rw', isa => 'Str', );
 
-has first => ( is => 'rw', isa => 'Str|Undef', clearer => 'clear_first', predicate => 'has_first' );
+has first => ( is => 'rw', isa => 'Str', clearer => 'clear_first', predicate => 'has_first' );
 
 1;
