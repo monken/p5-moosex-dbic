5920949b42ddc55ef206f964dcb2a516d1e31a71
Moritz Onken <onken@netcubed.de>
2011-01-31 15:00:03 +0100

    documentation

--- lib/MooseX/DBIC/Meta/Role/Attribute.pm
+++ lib/MooseX/DBIC/Meta/Role/Attribute.pm
@@ -21,3 +21,18 @@ after _process_options => sub {
 sub apply_to_result_source {}
 
 1;
+
+
+__END__
+
+=head1 INTERNAL METHODS
+
+=head2 _process_options
+
+Attributes that are lazy and do not have a builder or default set become lazy and
+a default assigned, that dies when called. This enables result classes to have
+incomplete data.
+
+=head1 ROLES
+
+Does role L<MooseX::Attribute::LazyInflator::Meta::Role::Attribute>.
